b73a0d09e8b75a6d3f7bb106f7f07d023f9e192f
Ruslan <russlik88@gmail.com>
2021-02-07 00:55:51 +0100

    added React form hooks

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,21 +1,23 @@
+// @ts-nocheck
 /** @format */
 
 import React, { useState } from 'react';
 import ContactForm from './form.js';
 import NumberFormat from 'react-number-format';
+import { useForm } from "react-hook-form";
 import { db } from '../firebase';
 
 import { Button } from '../../assets/css/basic';
 
 const Form = () => {
+	const {errors, register, handleSubmit} = useForm();
 	const [name, setName] = useState('');
 	const [companyName, setCompanyName] = useState('');
 	const [email, setEmail] = useState('');
 	const [message, setMessage] = useState('');
 	const [phone, setPhone] = useState('');
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+	const onSubmit = () => {
 
 		db.collection('Contact form Fortuna Blitz')
 			.add({
@@ -27,22 +29,20 @@ const Form = () => {
 			})
 			.then(() => {
 				// @ts-ignore
-			
 				document.querySelector('.success').classList.add('active');
 				setTimeout(() => {
 					document.querySelector('.success').classList.remove('active');
 				  }, 5000);
-				
 			})
 			.catch((error) => {
 				alert(error.message);
 			});
 
-		setName('');
-		setCompanyName('');
-		setEmail('');
-		setMessage('');
-		setPhone('');
+			setName('');
+			setCompanyName('');
+			setEmail('');
+			setMessage('');
+			setPhone('');
 	};
 
 	return (
@@ -51,26 +51,34 @@ const Form = () => {
 			<p className="address-block__paragraph">
 				Harjumaa, Tallinn linn, Narva mnt 13a, 10151
 			</p>
-			<ContactForm className="contact-form" onSubmit={handleSubmit}>
+			<ContactForm className="contact-form" onSubmit={handleSubmit(onSubmit)}>
 				<input
 					type="text"
+					name="name"
 					placeholder="Your name"
 					value={name}
+					ref={register({ required: true })}
 					onChange={(e) => setName(e.target.value)}
 				/>
+				{errors.name && <span style={{color: "#bf1650"}}>This field is required</span>}
 				<input
 					type="text"
+					name="companyName"
 					placeholder="Full company name"
 					value={companyName}
+					ref={register({ required: true })}
 					onChange={(e) => setCompanyName(e.target.value)}
 				/>
-
+				{errors.companyName && <span style={{color: "#bf1650"}}>This field is required</span>}
 				<input
 					type="email"
+					name="mail"
 					placeholder="Email"
 					value={email}
+					ref={register({ required: true,  pattern: /^\S+@\S+$/i })}
 					onChange={(e) => setEmail(e.target.value)}
 				/>
+				{errors.mail && <span style={{color: "#bf1650"}}>Please provide a valid email address</span>}
 
 				<label htmlFor="phone">Phone number</label>
 
@@ -78,25 +86,26 @@ const Form = () => {
 					id="phone"
 					format="+##(###) ###-####"
 					type="tel"
+					name="phone_number"
 					placeholder="+__(___) ___-____"
 					value={phone}
 					onChange={(e) => setPhone(e.target.value)}
+					ref={register({ required: true })}
 					allowEmptyFormatting
 					mask="_"
 				/>
+				{errors.phone_number && <span style={{color: "#bf1650"}}>This field is required</span>}
 
 				<textarea
 					placeholder="Message"
+					name="message"
 					value={message}
+					ref={register({ required: true })}
 					onChange={(e) => setMessage(e.target.value)}
 				></textarea>
+				{errors.message && <span style={{color: "#bf1650"}}>This field is required</span>}
+
 					<div className="success">Message submitted succesfuly!</div>
-				{/* <input
-					type="tel"
-					placeholder="(___) ___-____"
-					value={phone}
-					onChange={(e) => setPhone(e.target.value)}
-				/> */}
 				<Button type="submit" className="secondary secondary--modify --contact">
 					Submit
 				</Button>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, {lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import {GloabalStyles} from './assets/css/basic';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Preloader from './components/preloader/preloader.jsx';
 const App = lazy(() => import('./App'));
@@ -9,7 +8,6 @@ const App = lazy(() => import('./App'));
 
 ReactDOM.render(
   <React.StrictMode >
-      <GloabalStyles />
       <Suspense fallback={<Preloader />}>
         <Router>
             <App />
